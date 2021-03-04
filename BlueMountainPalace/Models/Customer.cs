39bbea272dda1100da3550d6ff05bd98fafbf947
Lylantz <pphantasm@gmail.com>
2021-03-05 02:12:32 +1100

    Create the basic model structures

---
+++ BlueMountainPalace/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMountainPalace.Models
+{
+    public class Customer
+    {
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Postcode { get; set; }
+        public ICollection<Booking> TheBookings { get; set; }
+    }
+}
---
+++ BlueMountainPalace/Models/Room.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMountainPalace.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Level { get; set; }
+        public int BedCount { get; set; }
+        public decimal Price { get; set; }
+        public ICollection<Booking> TheBookings { get; set; }
+    }
+}
