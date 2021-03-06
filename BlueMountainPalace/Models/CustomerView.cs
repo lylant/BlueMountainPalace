f3a0608a091e150ae8c69a0fc4e6561607310e42
Lylantz <pphantasm@gmail.com>
2021-03-06 16:58:52 +1100

    Set data annotations for CustomerView

--- BlueMountainPalace/Models/CustomerView.cs
+++ BlueMountainPalace/Models/CustomerView.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +9,25 @@ namespace BlueMountainPalace.Models
 {
     public class CustomerView
     {
+        [Required]
+        [MinLength(2), MaxLength(20)]
+        // RegEx: must start with a capital letter, then allows lower-case letters, apostrophe or hyphen
+        [RegularExpression(@"^[A-Z][a-z'-]*$")]
         public string LastName { get; set; }
+
+        [Required]
+        [MinLength(2), MaxLength(20)]
+        // RegEx: must start with a capital letter, then allows lower-case letters, apostrophe or hyphen
+        [RegularExpression(@"^[A-Z][a-z'-]*$")]
         public string FirstName { get; set; }
+
+        [NotMapped] // not mapping this property to the database, but will exist in memory
         public string FullName => $"{LastName} {FirstName}";
 
+        [Required]
+        [MinLength(4), MaxLength(4)]
+        // RegEx: only numeric characters are allowed
+        [RegularExpression(@"^[0-9]*$")]
         public string Postcode { get; set; }
     }
 }
