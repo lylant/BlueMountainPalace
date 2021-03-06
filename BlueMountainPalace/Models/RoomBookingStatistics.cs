8c7d74e74835bed7bd5dce2002f59015b5327d88
Lylantz <51321187+Lylantz@users.noreply.github.com>
2021-03-06 20:12:24 +1100

    Implement "Statistics" feature (#9)
    * Create CustomerStatistics
    * Create RoomBookingStatistics
    * Create a controller for Statistics feature
    * Create a view for Statistics feature

---
+++ BlueMountainPalace/Models/RoomBookingStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMountainPalace.Models
+{
+    public class RoomBookingStatistics
+    {
+        [Display(Name = "Room ID")]
+        public int RoomID { get; set; }
+
+        [Display(Name = "Number of Bookings")]
+        public int BookingCount { get; set; }
+    }
+}
