858d7b26423cc50ec2dd4e35ffb5ef6c2ff50c7c
Lylantz <pphantasm@gmail.com>
2021-03-06 16:56:13 +1100

    Create a CustomerView

---
+++ BlueMountainPalace/Models/CustomerView.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMountainPalace.Models
+{
+    public class CustomerView
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string FullName => $"{LastName} {FirstName}";
+
+        public string Postcode { get; set; }
+    }
+}
