8791f92e2e5def26811e81a11e0e241df492e6ff
Lylantz <pphantasm@gmail.com>
2021-03-06 19:57:03 +1100

    Create CustomerStatistics

---
+++ BlueMountainPalace/Models/CustomerStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMountainPalace.Models
+{
+    public class CustomerStatistics
+    {
+        [Display(Name = "Postcode")]
+        public string Postcode { get; set; }
+
+        [Display(Name = "Number of Customers")]
+        public int CustomerCount { get; set; }
+    }
+}
