efcc2f4eef0429460b2ea2a6426b21d5edbdbd69
Lylantz <pphantasm@gmail.com>
2021-03-06 18:37:18 +1100

    Create a BookingView

---
+++ BlueMountainPalace/Models/BookingView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlueMountainPalace.Models
+{
+    public class BookingView
+    {
+        public int RoomID { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+    }
+}
