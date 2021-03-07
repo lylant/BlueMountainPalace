8b534a63e7bc0fae7a51da9137c55c5883576d45
Lylantz <pphantasm@gmail.com>
2021-03-07 21:50:51 +1100

    Add Display attributes for some properties of Booking, Customer

--- BlueMountainPalace/Models/Booking.cs
+++ BlueMountainPalace/Models/Booking.cs
@@ -18,14 +18,17 @@ namespace BlueMountainPalace.Models
         [DataType(DataType.EmailAddress)]
         public string CustomerEmail { get; set; }
 
+        [Display(Name = "Check-In Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CheckIn { get; set; }
 
+        [Display(Name = "Check-Out Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime CheckOut { get; set; }
 
+        [Display(Name = "Amount Due")]
         [DataType(DataType.Currency)]
         public decimal Cost { get; set; }
 
